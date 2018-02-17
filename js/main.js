08366ed788483f4e09fa435d7576c7ec861a8db9
Vlad Ivanitskiy <ivanitskiyvv32@gmail.com>
2018-02-17 19:42:38 +0200

    five

--- js/main.js
+++ js/main.js
@@ -1,244 +1,208 @@
-$(document).ready(function() {
-	
-	setTimeout(function(){
-		$('body').addClass('loaded');
-		$('h1').css('color','#222222');
-	}, 3000);
-	
-});
+jQuery(document).ready(function($){
+	//open/close lateral filter
+	$('.cd-filter-trigger').on('click', function(){
+		triggerFilter(true);
+	});
+	$('.cd-filter .cd-close').on('click', function(){
+		triggerFilter(false);
+	});
 
-$(function() {
-	
-	if (document.createElement('canvas').getContext) {
-		var image = $('<img />').load(function() {
-			var image = this,
-				offset_left = 5,
-				offset_number = 4;
-			
-			function drawNumberLine(context, position, number, line) {
-				context.drawImage(image, number * 25, line, 25, 1, position * 25 + offset_left + offset_number, line, 25, 1);
-			}
-			
-			function drawNumberBottom(context, position, number) {
-				context.drawImage(image, number * 25, 25, 25, 23, position * 25 + offset_left + offset_number, 25, 25, 23);
-			}
-			
-			function drawNumberTop(context, position, number) {
-				context.drawImage(image, number * 25, 0, 25, 23, position * 25 + offset_left + offset_number, 0, 25, 23);
-			}
-			
-			function cf_draw(from, to, canvas, step) {
-				var context = canvas.getContext('2d'),
-					offsets = [
-					         {w: 2,		h: -2},
-					         {w: 4,		h: -5},
-					         {w: 6,		h: -9},
-					         {w: 8,		h: -14},
-					         {w: 10,	h: -20},
-					         {w: 0,		h:  0},
-					         {w: 10,	h: -20},
-					         {w: 8,		h: -14},
-					         {w: 6,		h: -9},
-					         {w: 4,		h: -5},
-					         {w: 2,		h: -2},
-					         {w: 0,		h:  0}
-					],
-					width = canvas.width,
-					nwidth = width + offsets[step].w,
-					maxscale = nwidth/width,
-					height = 23,
-					nheight = height + offsets[step].h,
-					scale_y = nheight / height,
-					dig_col = parseInt($(canvas).parents('div.c-block')[0].className.substr(-1), 10);
-				
-				from = from.toString();
-				to = to.toString();
-				
-				while (from.length < dig_col) {from = '0' + from;}
-				while (to.length < dig_col) {to = '0' + to;}
-				
-				
-				context.clearRect(0, 0, width, canvas.height);
-				context.save();
-	
-				for (var j = 0; j <= to.length - 1; j++) {
-					drawNumberTop(context, j, to[j]);
-				}
-				
-				if (step == 11) {
-					for (var j = 0; j <= to.length - 1; j++) {
-						drawNumberBottom(context, j, to[j]);
-					}
-					return;
-				}
-				
-				
-				for (var j = 0; j <= from.length - 1; j++) {
-					drawNumberBottom(context, j, from[j]);
-				}
-	
-				// draw bg
-				context.drawImage(image, 0, 96 + step * 48, width - 5, 48, 0, 0, width - 5, 48);
-				context.drawImage(image, 125, 96 + step * 48, 4, 48, width - 5, 0, 5, 48);
-	
-				for (var i = 0; i < 24; i++) {
-					if (step < 5) {
-						context.setTransform(maxscale - (maxscale - 1) * i / 23, 0, 0, scale_y, -(nwidth - width) / 2 * (1 - i / 23), -offsets[step].h);
-						for (var j = 0; j <= from.length - 1; j++) {
-							drawNumberLine(context, j, from[j], i);
-						}
-					}
-					
-					if (step > 5) {
-						context.setTransform(1 + (maxscale - 1) * i / 23, 0, 0, scale_y, -(nwidth - width) / 2 * (i / 23), -offsets[step].h);
-						for (var j = 0; j <= to.length - 1; j++) {
-							drawNumberLine(context, j, to[j], 25+i);
-						}
-					}
-					context.restore();
-				}
-				context.setTransform(1, 0, 0, 1, 0, 0);
-	
-				// draw blick
-				context.drawImage(image, 130, 96 + step * 48, width - 5, 48, 0, 0, width - 5, 48);
-				context.drawImage(image, 255, 96 + step * 48, 4, 48, width -5, 0, 5, 48);
-			}
-				
-			function cf_animate(from, to, canvas, step) {
-				cf_draw(from, to, canvas, step);
-				step ++;
-				if (step > 11) return;
-	
-				setTimeout(function() {
-					cf_animate(from, to, canvas, step);
-				}, 50 - 20 * step/11);
-			}
-			
-			function seconds(from, canvas) {
-				var to = from - 1;
-				
-				if (from <= 0) {
-					to = 59;
-					
-					// minutes animate
-					var minutes_from = parseInt($('span', minutes_cnt).text(), 10);
-					var minutes_to = minutes_from - 1;
-					
-					if (minutes_from <= 0) {
-						minutes_to = 59;
-						
-						// hourses animate
-						var hourses_from = parseInt($('span', hourses_cnt).text(), 10);
-						var hourses_to = hourses_from - 1;
-						
-						if (hourses_from <= 0) {
-							hourses_to = 23;
-							
-							var days_from = parseInt($('span', days_cnt).text(), 10);
-							var days_to = days_from - 1;
-							
-							if (days_from == 0) {
-								return;
-							}
-							cf_animate(days_from, days_to, $('canvas', days_cnt)[0], 0);
-							$('span', days_cnt).text(days_to);
-						}
-						cf_animate(hourses_from, hourses_to, $('canvas', hourses_cnt)[0], 0);
-						$('span', hourses_cnt).text(hourses_to);
-					}
-	
-					cf_animate(minutes_from, minutes_to, $('canvas', minutes_cnt)[0], 0);
-					$('span', minutes_cnt).text(minutes_to);
-				}
-			
-				cf_animate(from, to, canvas, 0);
-				
-				from--;
-				if (from < 0) {from = 59;}
-	
-				setTimeout(function() {
-					seconds(from, canvas);
-				}, 1000);
-			}
-			
-			
-			$('div.bl-inner').each(function() {
-				var canvas = $('<canvas></canvas>').css({
-					position: 'absolute',
-					left: -5,
-					top: 0
-				}).appendTo(this)[0];
-				
-				canvas.width = $(this).width() + offset_left * 2 + 3;
-				canvas.height = $(this).height();
-				
-				cf_draw(parseInt($('span', this).text(), 10), parseInt($('span', this).text(), 10), canvas, 11);
-			});
-			
-			var seconds_cnt = $('div.time .bl-inner:last'),
-				minutes_cnt = $('div.time .bl-inner:eq(2)'),
-				hourses_cnt = $('div.time .bl-inner:eq(1)'),
-				days_cnt 	= $('div.time .bl-inner:eq(0)');
-	
-			seconds(parseInt($('span', seconds_cnt).text(), 10), $('canvas', seconds_cnt)[0]);
+	function triggerFilter($bool) {
+		var elementsToTrigger = $([$('.cd-filter-trigger'), $('.cd-filter'), $('.cd-tab-filter'), $('.cd-gallery')]);
+		elementsToTrigger.each(function(){
+			$(this).toggleClass('filter-is-visible', $bool);
 		});
-		image.attr('src', 'i/sprites.png');
-	} else {
-		$('div.bl-inner span').show();
 	}
+
+	//mobile version - detect click event on filters tab
+	var filter_tab_placeholder = $('.cd-tab-filter .placeholder a'),
+		filter_tab_placeholder_default_value = 'Select',
+		filter_tab_placeholder_text = filter_tab_placeholder.text();
+	
+	$('.cd-tab-filter li').on('click', function(event){
+		//detect which tab filter item was selected
+		var selected_filter = $(event.target).data('type');
+			
+		//check if user has clicked the placeholder item
+		if( $(event.target).is(filter_tab_placeholder) ) {
+			(filter_tab_placeholder_default_value == filter_tab_placeholder.text()) ? filter_tab_placeholder.text(filter_tab_placeholder_text) : filter_tab_placeholder.text(filter_tab_placeholder_default_value) ;
+			$('.cd-tab-filter').toggleClass('is-open');
+
+		//check if user has clicked a filter already selected 
+		} else if( filter_tab_placeholder.data('type') == selected_filter ) {
+			filter_tab_placeholder.text($(event.target).text());
+			$('.cd-tab-filter').removeClass('is-open');	
+
+		} else {
+			//close the dropdown and change placeholder text/data-type value
+			$('.cd-tab-filter').removeClass('is-open');
+			filter_tab_placeholder.text($(event.target).text()).data('type', selected_filter);
+			filter_tab_placeholder_text = $(event.target).text();
+			
+			//add class selected to the selected filter item
+			$('.cd-tab-filter .selected').removeClass('selected');
+			$(event.target).addClass('selected');
+		}
+	});
+	
+	//close filter dropdown inside lateral .cd-filter 
+	$('.cd-filter-block h4').on('click', function(){
+		$(this).toggleClass('closed').siblings('.cd-filter-content').slideToggle(300);
+	})
+
+	//fix lateral filter and gallery on scrolling
+	$(window).on('scroll', function(){
+		(!window.requestAnimationFrame) ? fixGallery() : window.requestAnimationFrame(fixGallery);
+	});
+
+	function fixGallery() {
+		var offsetTop = $('.cd-main-content').offset().top,
+			scrollTop = $(window).scrollTop();
+		( scrollTop >= offsetTop ) ? $('.cd-main-content').addClass('is-fixed') : $('.cd-main-content').removeClass('is-fixed');
+	}
+
+	/************************************
+		MitItUp filter settings
+		More details: 
+		https://mixitup.kunkalabs.com/
+		or:
+		http://codepen.io/patrickkunka/
+	*************************************/
+
+	buttonFilter.init();
+	$('.cd-gallery ul').mixItUp({
+	    controls: {
+	    	enable: false
+	    },
+	    callbacks: {
+	    	onMixStart: function(){
+	    		$('.cd-fail-message').fadeOut(200);
+	    	},
+	      	onMixFail: function(){
+	      		$('.cd-fail-message').fadeIn(200);
+	    	}
+	    }
+	});
+
+	//search filtering
+	//credits http://codepen.io/edprats/pen/pzAdg
+	var inputText;
+	var $matching = $();
+
+	var delay = (function(){
+		var timer = 0;
+		return function(callback, ms){
+			clearTimeout (timer);
+		    timer = setTimeout(callback, ms);
+		};
+	})();
+
+	$(".cd-filter-content input[type='search']").keyup(function(){
+	  	// Delay function invoked to make sure user stopped typing
+	  	delay(function(){
+	    	inputText = $(".cd-filter-content input[type='search']").val().toLowerCase();
+	   		// Check to see if input field is empty
+	    	if ((inputText.length) > 0) {            
+	      		$('.mix').each(function() {
+		        	var $this = $(this);
+		        
+		        	// add item to be filtered out if input text matches items inside the title   
+		        	if($this.attr('class').toLowerCase().match(inputText)) {
+		          		$matching = $matching.add(this);
+		        	} else {
+		          		// removes any previously matched item
+		          		$matching = $matching.not(this);
+		        	}
+	      		});
+	      		$('.cd-gallery ul').mixItUp('filter', $matching);
+	    	} else {
+	      		// resets the filter to show all item if input is empty
+	      		$('.cd-gallery ul').mixItUp('filter', 'all');
+	    	}
+	  	}, 200 );
+	});
 });
-jQuery( function( $ ) {
- var $window = $( window ); // 1. Объект Window.
-var $featuredMedia = $( "#featured-media" ); // 1. Контейнер видео.
-var $featuredVideo = $( "#featured-video" ); // 2. Видео с Youtube.
- 
-var player; // 3. Объект Youtube плеера.
-var top = $featuredMedia.offset().top; // 4. Отступ видео сверху документа;
-var offset = Math.floor( top + ( $featuredMedia.outerHeight() / 2 ) ); //5. offset.
-window.onYouTubeIframeAPIReady = function() {
-player = new YT.Player( "featured-video", {
-   events: {
-     "onStateChange": onPlayerStateChange
-   }
-} );
+
+/*****************************************************
+	MixItUp - Define a single object literal 
+	to contain all filter custom functionality
+*****************************************************/
+var buttonFilter = {
+  	// Declare any variables we will need as properties of the object
+  	$filters: null,
+  	groups: [],
+  	outputArray: [],
+  	outputString: '',
+  
+  	// The "init" method will run on document ready and cache any jQuery objects we will need.
+  	init: function(){
+    	var self = this; // As a best practice, in each method we will asign "this" to the variable "self" so that it remains scope-agnostic. We will use it to refer to the parent "buttonFilter" object so that we can share methods and properties between all parts of the object.
+    
+    	self.$filters = $('.cd-main-content');
+    	self.$container = $('.cd-gallery ul');
+    
+	    self.$filters.find('.cd-filters').each(function(){
+	      	var $this = $(this);
+	      
+		    self.groups.push({
+		        $inputs: $this.find('.filter'),
+		        active: '',
+		        tracker: false
+		    });
+	    });
+	    
+	    self.bindHandlers();
+  	},
+  
+  	// The "bindHandlers" method will listen for whenever a button is clicked. 
+  	bindHandlers: function(){
+    	var self = this;
+
+    	self.$filters.on('click', 'a', function(e){
+	      	self.parseFilters();
+    	});
+	    self.$filters.on('change', function(){
+	      self.parseFilters();           
+	    });
+  	},
+  
+  	parseFilters: function(){
+	    var self = this;
+	 
+	    // loop through each filter group and grap the active filter from each one.
+	    for(var i = 0, group; group = self.groups[i]; i++){
+	    	group.active = [];
+	    	group.$inputs.each(function(){
+	    		var $this = $(this);
+	    		if($this.is('input[type="radio"]') || $this.is('input[type="checkbox"]')) {
+	    			if($this.is(':checked') ) {
+	    				group.active.push($this.attr('data-filter'));
+	    			}
+	    		} else if($this.is('select')){
+	    			group.active.push($this.val());
+	    		} else if( $this.find('.selected').length > 0 ) {
+	    			group.active.push($this.attr('data-filter'));
+	    		}
+	    	});
+	    }
+	    self.concatenate();
+  	},
+  
+  	concatenate: function(){
+    	var self = this;
+    
+    	self.outputString = ''; // Reset output string
+    
+	    for(var i = 0, group; group = self.groups[i]; i++){
+	      	self.outputString += group.active;
+	    }
+    
+	    // If the output string is empty, show all rather than none:    
+	    !self.outputString.length && (self.outputString = 'all'); 
+	
+    	// Send the output string to MixItUp via the 'filter' method:    
+		if(self.$container.mixItUp('isLoaded')){
+	    	self.$container.mixItUp('filter', self.outputString);
+		}
+  	}
 };
-
-/**
- * Запускается, когда меняется состояние Youtube видео (воспроизведение, пауза и т.д.).
- *
- * @param {Object} event The Youtube Object Event.
- * @return {Void}
- */
-function onPlayerStateChange( event ) {
- 
-   var isPlay  = 1 === event.data;
-   var isPause = 2 === event.data;
-   var isEnd   = 0 === event.data;
- 
-   if ( isPlay ) {
-      $featuredVideo.removeClass( "is-paused" );
-      $featuredVideo.toggleClass( "is-playing" );
-   }
- 
-   if ( isPause ) {
-      $featuredVideo.removeClass( "is-playing" );
-      $featuredVideo.toggleClass( "is-paused" );
-   }
- 
-   if ( isEnd ) {
-      $featuredVideo.removeClass( "is-playing", "is-paused" );
-   }
-}
-
-$window
-.on( "resize", function() {
-   top = $featuredMedia.offset().top;
-   offset = Math.floor( top + ( $featuredMedia.outerHeight() / 2 ) );
-} )
- 
-.on( "scroll", function() {
-   $featuredVideo.toggleClass( "is-sticky",
-     $window.scrollTop() > offset && $featuredVideo.hasClass( "is-playing" )
-   );
-} );
-} );
