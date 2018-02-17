d974d18c18d3564f76a56b75d0a138496ff77837
Vlad Ivanitskiy <ivanitskiyvv32@gmail.com>
2018-02-17 18:38:22 +0200

    Two

---
+++ blocks/blog-all.php
@@ -0,0 +1,8 @@
+<div class="possible">
+			<div class="container">
+				<div class="possible-top">
+					<h3>БЛОГ</h3>
+					<img class="img-responsive line-grid" src="images/li-1.png" alt=""/> 
+				</div>
+			</div>
+		</div>
---
+++ blocks/form.php
@@ -0,0 +1,24 @@
+
+					<div class="contact-grid">
+						<div class="col-md-6 contact-us">
+							<input type="text" placeholder="Ваше имя" id="name" name="name" required ><br />
+						</div>
+						<div class="col-md-6 contact-us">
+							<input type="text" placeholder="Email" id="email" name="email"><br />
+						</div>
+						<div class="clearfix"> </div>
+					</div>
+					<div class="contact-grid">
+						<div class="col-md-6 contact-us">
+							<input type="text" placeholder="URL" id="url" url="url"><br />
+						</div>
+						<div class="col-md-6 contact-us">
+							<input type="text" placeholder="Тема" id="subject" subject="subject"><br />
+						</div>
+						<div class="clearfix"> </div>
+					</div>
+					<textarea cols="77" rows="6" value=" " id="massage" massage="massage"></textarea>
+					<div class="send">
+						<input type="submit" value="Надіслати" >
+					</div>
+			
